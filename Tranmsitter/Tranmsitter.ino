072cf6bbad42e166a11bf5041523a384a23713d9
Joel Veloz <joevpazm@espol.edu.ec>
2022-04-25 12:14:23 +0300

    Base

---
+++ Tranmsitter/Tranmsitter.ino
@@ -0,0 +1,55 @@
+
+/* Tranmsitter code for the Arduino Radio control with PWM output
+    Module // Arduino UNO,NANO
+    GND    ->   GND
+    Vcc    ->   3.3V
+    CE     ->   D9
+    CSN    ->   D10
+    CLK    ->   D13
+    MOSI   ->   D11
+    MISO   ->   D12
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+const uint64_t my_radio_pipe = 0xE8E8F0F0E1LL; //Remember that this code should be the same for the receiver
+
+RF24 radio(9, 10);
+
+// The sizeof this struct should not exceed 32 bytes
+struct Data_to_be_sent {
+  byte ch1; 
+};
+
+Data_to_be_sent sent_data;
+
+
+
+void setup()
+{
+   Serial.begin(9600); 
+  radio.begin();
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openWritingPipe(my_radio_pipe);  
+  sent_data.ch1 = 127;
+  
+}
+
+/**************************************************/
+
+
+void loop()
+{
+  /*If your channel is reversed, just swap 0 to 255 by 255 to 0 below
+  EXAMPLE:
+  Normal:    data.ch1 = map( analogRead(A0), 0, 1024, 0, 255);
+  Reversed:  data.ch1 = map( analogRead(A0), 0, 1024, 255, 0);  */
+  Serial.println(analogRead(A0));
+  Serial.print(radio.available());
+  sent_data.ch1 = map( analogRead(A0), 0, 1024, 0, 255);
+
+ radio.write(&sent_data, sizeof(Data_to_be_sent));
+}
